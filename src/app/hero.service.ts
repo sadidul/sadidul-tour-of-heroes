ab964687f7400c52aa07fdad3e0a000804b022fe
Sadidul Kabir <sadidul.kabir@hishab.co>
2020-11-04 13:23:45 +0600

    Tour of Heroes:Add In-app Navigation done

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,9 @@
-/* tslint:disable:no-trailing-whitespace */
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -13,8 +14,12 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
